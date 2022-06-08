47821bf233ee2d82a1eeeeea6fe81e4da8e946ab
millergallegof <miller.gallegof@gmail.com>
2022-06-08 15:20:45 -0500

    Creacion repositorio roles

---
+++ src/main/java/com/crud/democrud/repositories/RolesModelRepository.java
@@ -0,0 +1,7 @@
+package com.crud.democrud.repositories;
+
+import com.crud.democrud.models.RolesModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RolesModelRepository extends CrudRepository<RolesModel, Long> {
+}
